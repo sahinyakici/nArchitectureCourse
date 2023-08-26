3975ef67f78cefd7f23c89467da3e16ec4ef0a46
Sahin YAKICI <shnykc@hotmail.com>
2023-08-26 23:43:08 +0300

    feat: first section was ended.
    First section was completed and implemented.

---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using Core.Packages.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public String Name { get; set; }
+
+    public Brand()
+    {
+    }
+
+    public Brand(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
