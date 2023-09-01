f834e5872e833a6bdbbed409c5618e6a75d3eb2c
Sahin YAKICI <shnykc@hotmail.com>
2023-09-01 13:56:14 +0300

    feat: Eighteen section was ended
    Redis implementation was completed.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
-builder.Services.AddDistributedMemoryCache();
+
+//builder.Services.AddDistributedMemoryCache();
+builder.Services.AddStackExchangeRedisCache(opt => opt.Configuration = "localhost:6379");
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
